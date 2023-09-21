94e0c5f1212a243af000193fffcf06786d3208a1
james911256 <james911256@gmail.com>
2023-09-21 10:02:43 +0900

    소년,잔디 그리기

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -1,3 +1,4 @@
+#Agile Software Development 에자일 코딩
 from pico2d import *
 
 open_canvas()
@@ -5,19 +6,27 @@ open_canvas()
 glass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_circle(): #pass : 아무것도 안하고 넘김
+def move_circle(): 
     print('CIRCLE')
+
+    #일단 그림을 그리자
+    clear_canvas_now()
+    glass.draw_now(400,30)
+    character.draw_now(400,90)
+    delay(1)
+    
     pass
 
 def move_rectangle():
     print('rectangle')
     pass
-    
+
+
 
 while True:
     move_circle()
     move_rectangle()
-
+    break
     
 close_canvas()
 

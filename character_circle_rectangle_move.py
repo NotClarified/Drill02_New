0508e3510c77abd3ada50101b0626764a8325e55
james911256 <james911256@gmail.com>
2023-09-21 09:58:15 +0900

    원, 삼각형 무한루프

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -5,4 +5,19 @@ open_canvas()
 glass = load_image('grass.png')
 character = load_image('character.png')
 
+def move_circle(): #pass : 아무것도 안하고 넘김
+    print('CIRCLE')
+    pass
+
+def move_rectangle():
+    print('rectangle')
+    pass
+    
+
+while True:
+    move_circle()
+    move_rectangle()
+
+    
 close_canvas()
+

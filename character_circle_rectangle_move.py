fea630d2d44b9cc9e0fcada6068fc52c6bdd9569
james911256 <james911256@gmail.com>
2023-09-21 10:37:48 +0900

    retengle 윗 line

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -7,6 +7,12 @@ open_canvas()
 glass = load_image('grass.png')
 character = load_image('character.png')
 
+def render_frame(x,y):
+    clear_canvas_now()
+    glass.draw_now(400,30)
+    character.draw_now(x, y)
+    delay(0.01)
+    
 def move_circle(): 
     
     #일단 그림을 그리자
@@ -15,24 +21,18 @@ def move_circle():
     for degree in range(0,360,5):
         x = cx + r * math.cos(math.radians(degree))
         y = cy + r * math.sin(math.radians(degree))
-        clear_canvas_now()
-        glass.draw_now(400,30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_frame(x,y)
 
 def move_rectangle():
     for x in range(50,750+1,10):
-        clear_canvas_now()
-        glass.draw_now(400,30)
-        character.draw_now(x, 90)
-        delay(0.01)    
-    
-    pass
+        render_frame(x,90)
+        
 
-
-
+    for x in range(750,50-1,-10):
+        render_frame(x,90)
+        
 while True:
-    # move_circle()
+    #move_circle()
     move_rectangle()
     break
     
